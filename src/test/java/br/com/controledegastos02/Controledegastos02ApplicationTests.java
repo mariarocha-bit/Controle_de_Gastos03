6a39cfe6f219ede1ff8b6f3150c28765b7511886
mariarocha-bit <mahrocha008@gmail.com>
2026-05-25 23:51:28 -0300

    Add files via upload

---
+++ src/test/java/br/com/controledegastos02/Controledegastos02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controledegastos02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controledegastos02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/br/com/controledegastos2/Controledegastos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controledegastos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controledegastos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
